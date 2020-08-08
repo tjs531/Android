a6c9bd588ed5ac6e1f68c7eed842ddafa802bf3a
tjs531 <tjs531@naver.com>
2020-08-08 21:49:00 +0900

    2020-08-08

---
+++ MyTravel/app/src/main/java/com/example/mytravel/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.mytravel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import net.daum.mf.map.api.MapView;
+import android.view.ViewGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MapView mapView = new MapView(this);
+
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+    }
+}
